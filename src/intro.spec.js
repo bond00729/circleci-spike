4da3de665e09823d1557f0a54163b883b5d99028
Matthew Bond <matthew.randal.bond@gmail.com>
2019-02-13 12:05:04 -0800

    Made unit test fail

--- src/intro.spec.js
+++ src/intro.spec.js
@@ -2,6 +2,6 @@ const expect = require('chai').expect;
 
 describe('intro', () => {
   it('intro is intro', () => {
-    expect('intro').to.eql('intro');
+    expect('intro').to.eql('introoooo');
   });
 });
